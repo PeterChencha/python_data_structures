3a0e21e9b0456f11c282628f30321bf061d9ba8d
extraordinaire01 <peter@chenchatech.com>
2020-03-31 22:25:58 +0300

    Remove value at specific index

--- arrayimplementation.py
+++ arrayimplementation.py
@@ -28,6 +28,7 @@ class MyArray(object):
             return firstItem
 
     def removeLastValue(self):
+        #COMMONLY REFERED TO AS POP
         if self.checkIfEmpty():
             error = "Array is empty"
             return error
@@ -37,12 +38,25 @@ class MyArray(object):
             self.length = self.length - 1
             return lastItem
 
+    def deleteSpecific(self, index):
+        item = self.data[index]
+        self.shiftItems(index)
+        return item
+
+
+    def shiftItems(self, index):
+        for _ in range(index, (self.length - 1)):
+            self.data[_] = self.data[_ + 1]
+        del self.data[self.length - 1]
+        self.length = self.length - 1
+
 arr = MyArray()
-# arr.add(70)
-# arr.add(50)
-# arr.add(100)
+arr.add(70)
+arr.add(50)
+arr.add(100)
 #print (arr.random())
 #print (arr.get(2))
 #print(arr.removeLastValue())
-print(arr.removeFirstValue())
-#print (arr.length)
+#print(arr.removeFirstValue())
+print (arr.deleteSpecific(1))
+print (arr.length)
