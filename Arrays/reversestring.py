9896666bb2e89ef67d83cba33e3fca3d62fdcb5d
extraordinaire01 <peter@chenchatech.com>
2020-03-31 23:09:39 +0300

    Slicing worked best.

--- Arrays/reversestring.py
+++ Arrays/reversestring.py
@@ -4,6 +4,15 @@ def reverseString(string):
         backward.append(string[_])
     return ' '.join(backward)
 
+# test_string = "Hi my name is Peter"
+# results = reverseString(test_string)
+# print results
+
+def reverseBySlicing(string):
+    len_of_string = len(string)
+    slicedString = string[len_of_string::-1]
+    return slicedString
+
 test_string = "Hi my name is Peter"
-results = reverseString(test_string)
+results = reverseBySlicing(test_string)
 print results
