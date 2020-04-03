9260f2a6e5911faa61f9eb10badb59481c67e4af
extraordinaire01 <peter@chenchatech.com>
2020-04-03 22:06:31 +0300

    Transversal attempt.

--- Arrays/mergesortedarrays.py
+++ Arrays/mergesortedarrays.py
@@ -1,6 +1,6 @@
 import time
 def mergesortedarrays(arr1, arr2):
-    #OPERATION HAS O(nlogn)
+    #OPERATION HAS O(nlogn) TIME COMPLEXITY
     t = time.process_time()
     joined = arr1 + arr2
     joined.sort()
@@ -9,47 +9,44 @@ def mergesortedarrays(arr1, arr2):
     return joined
 
 
-# test_arr1 = [0,3,4,31]
-# test_arr2 = [4,6,30]
-#
-# results = mergesortedarrays(test_arr1, test_arr2)
-# print (results)
+test_arr1 = [0,3,4,31]
+test_arr2 = [4,6,30]
+
+results = mergesortedarrays(test_arr1, test_arr2)
+print (results)
 
 def mergesortedarraysFaster(arr1,arr2):
     #FUNCTION IS O(N) - Though in my time analysis is seems to take longer
+    #EDITED = NOW NOT RUNNING
     t = time.process_time()
     joined_array = [None] * (len(arr1)+len(arr2))
-    i = 0
-    j = 0
-    k = 0
+    #CHECK INPUT
+    if (len(arr1) == 0):
+        return arr2
+    if (len(arr2) == 0):
+        return arr1
+
+    item_arr1 = arr1[0]
+    item_arr2 = arr2[0]
+    i = 1
+    j = 1
 
     #TRANSVERSE BOTH ARRAYS
-    while i < len(arr1) and j < len(arr2):
-        if arr1[i] > arr2[j]:
-            joined_array[k] = arr1[i]
-            k = k + 1
-            i = i + 1
+    while (item_arr1 or item_arr2):
+        if (item_arr2 is None or item_arr1 < item_arr2):
+            joined_array.append(item_arr1)
+            item_arr1 = arr1[i]
+            i += 1
         else:
-            joined_array[k] = arr2[j]
-            k = k + 1
-            j = j + 1
-    while i < len(arr1):
-        joined_array[k] = arr1[i];
-        k = k + 1
-        i = i + 1
-
-    # Store remaining elements
-    # of second array
-    while j < len(arr2):
-        joined_array[k] = arr2[j];
-        k = k + 1
-        j = j + 1
+            joined_array.append(item_arr2)
+            item_arr2 = arr2[j]
+            j += 1
     elapsed_time = time.process_time() - t
     print (elapsed_time)
     return joined_array
 
-test_arr1 = [0,3,4,31]
-test_arr2 = [4,6,30]
-
-results = mergesortedarraysFaster(test_arr1, test_arr2)
-print (results)
+# test_arr1 = [0,3,4,31]
+# test_arr2 = [3,4,6,30]
+#
+# results = mergesortedarraysFaster(test_arr1, test_arr2)
+# print (results)
