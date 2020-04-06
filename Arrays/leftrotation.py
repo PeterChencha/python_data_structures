cc51fd86230cf0e5895c3584cde9af7c38e016a6
extraordinaire01 <peter@chenchatech.com>
2020-04-06 03:19:11 +0300

    left rotate array

---
+++ Arrays/leftrotation.py
@@ -0,0 +1,42 @@
+class LeftRotate(object):
+    def __init__(self, array, multiple):
+        self.array = array
+        self.multiple = multiple
+
+    def checkIfEmpty(self):
+        if len(self.array) == 0:
+            return True
+
+
+    def removeFirstValue(self):
+        if self.checkIfEmpty():
+            error = "Array is empty"
+            return error
+        else:
+            firstItem = self.array[0]
+            del self.array[0]
+            return firstItem
+
+    def addLastItem(self):
+        #COMMONLY REFERED TO AS POP
+        if self.checkIfEmpty():
+            error = "Array is empty"
+            return error
+        else:
+            lastItem = self.removeFirstValue()
+            self.array.append(lastItem)
+        return self.array
+
+    def leftRotateArray(self):
+        count = -1
+        while self.multiple > count:
+            self.addLastItem()
+            count = count + 1
+
+        return self.array
+
+test_arr = [5,1,2,3,4]
+multiple = 4
+my_array = LeftRotate(test_arr, multiple)
+result = my_array.leftRotateArray()
+print(result)
