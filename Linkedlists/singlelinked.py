63fb849e2db4ca7c8526f33d1a559b213369bc22
extraordinaire01 <peter@chenchatech.com>
2020-05-07 15:05:48 +0300

    Have a singly linked list with all functions.

--- Linkedlists/singlelinked.py
+++ Linkedlists/singlelinked.py
@@ -52,6 +52,26 @@ class LinkedList(object):
         else:
             previousNode.nextNode = currentNode.nextNode
 
+
+    def sortLinkedList(self):
+
+        actualNode = self.head
+        checkNode = None
+
+        if actualNode is None:
+            return
+
+        while actualNode is not None:
+            checkNode = actualNode.nextNode
+            while checkNode is not None:
+                if actualNode.data > checkNode.data:
+                    tempdata = actualNode.data
+                    actualNode.data = checkNode.data
+                    checkNode.data = tempdata
+                checkNode = checkNode.nextNode
+            actualNode = actualNode.nextNode
+
+
     def find(self, value):
 
         if self.head is None:
@@ -68,6 +88,34 @@ class LinkedList(object):
                 message = "{} was NOT found in the LinkedList".format(value)
                 return message
             currentNode = currentNode.nextNode
+
+    def removeDuplicates(self):
+        current = second = self.head
+        while current is not None:
+            while second.nextNode is not None:
+                if second.nextNode.data == current.data:
+                    self.size = self.size - 1
+                    second.nextNode = second.nextNode.nextNode
+                else:
+                    second = second.nextNode
+            current = second = current.nextNode
+
+    def getFromLast(self, pointer):
+        if self.head is None:
+            return
+        elif pointer > self.size:
+            error = "Invalid Pointer"
+            return error
+
+        no_of_nexts = self.size - pointer
+        actualNode = self.head
+        while no_of_nexts != 1:
+            actualNode = actualNode.nextNode
+            no_of_nexts = no_of_nexts - 1
+        value = actualNode.data
+        message = "The {} from last is {}".format(pointer, value)
+        return message
+    
 
     """O(N)"""
     def traverseList(self):
